fbbf15df06d7b8bf834e001484e0131acdeee965
ABrooksGit <Abrooks6211996@gmail.com>
2025-04-28 12:26:36 -0400

    Start of Project

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,140 @@
+package com.pluralsight;
+
+public class Main {
+
+    //Console Class
+    //Transaction Manager Class
+    //ArrayList
+    public static void main(String[] args) {
+
+
+    }
+
+
+
+
+
+
+
+    
+
+
+
+    //Home Screen Method
+
+
+
+
+
+
+
+
+    //Make A deposit Method
+
+
+
+
+
+
+
+
+
+
+    //Make A Payment Method
+
+
+
+
+
+
+    //Create A Ledger Screen
+
+
+
+
+
+
+
+
+
+    //Display Every Entry inside of Transaction txt
+
+
+
+
+
+
+    //Display All Deposits
+
+
+
+
+
+
+
+
+    //Display ALl Payments
+
+
+
+
+
+
+    //GO TO The Report Screen
+
+
+
+
+
+
+    //Methods for Month To Day
+
+
+
+
+    //Methods for Previous Month
+
+
+
+
+
+    //Methods for Year To Date
+
+
+
+
+
+    //Methods for the Previous Year
+
+
+
+
+    //Ability to Search Via Typing in the Vendor
+
+
+
+
+
+
+    //Format the time and dates to the log using TransactionManager Class
+
+
+
+
+    // Display The Format For The Time and Dates to the Console itself
+
+
+
+
+
+    //Method to display only the deposits
+
+
+
+
+
+
+    //Method to only display the payments
+
+
+}
